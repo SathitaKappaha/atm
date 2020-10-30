7f6a3aeee837669e1fea7b4bdeb7c386c0e2c94e
SathitaKappaha <72617585+SathitaKappaha@users.noreply.github.com>
2020-10-30 18:15:33 +0700

    Add files via upload

---
+++ src/main/java/th/ac/kmitl/atm/controller/HomeController.java
@@ -0,0 +1,18 @@
+package th.ac.kmitl.atm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+@RequestMapping("/home2")
+public  String getHomePage(Model model) {
+        model.addAttribute("greeting","Sawaddee");
+        return  "home";
+}
+
+
+
+
+}
---
+++ src/main/java/th/ac/kmitl/atm/service/Customerservice.java
@@ -0,0 +1,54 @@
+package th.ac.kmitl.atm.service;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+import th.ac.kmitl.atm.controller.model.Customer;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+
+        @Service
+        public class CustomerService {
+
+            private List<Customer> customerList;
+
+            @PostConstruct
+            public void postContruct() {
+                this.customerList = new ArrayList<>();
+            }
+
+            public void createCustomer(Customer customer) {
+
+                String hashPin = hash(customer.getPin());
+                customer.setPin(hashPin);
+                customerList.add(customer);
+            }
+
+            public List<Customer> getCustomers() {
+                return new ArrayList<>(this.customerList);
+            }
+            public Customer findCustomer(int id) {
+                for (Customer customer : customerList) {
+                    if (customer.getId() == id)
+                        return customer;
+                }
+                return null;
+            }
+            public Customer checkPin(Customer inputCustomer) {
+                Customer storedCustomer = findCustomer(inputCustomer.getId());
+                if (storedCustomer != null) {
+                    String hashPin = storedCustomer.getPin();
+
+                    if (BCrypt.checkpw(inputCustomer.getPin(), hashPin))
+                        return storedCustomer;
+                }
+
+            private String hash(String pin) {
+                String salt = BCrypt.gensalt(12);
+                return BCrypt.hashpw(pin, salt);
+
+        }
+
+
